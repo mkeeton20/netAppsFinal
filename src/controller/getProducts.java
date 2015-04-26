09792388a7a800b1d8f75833ca0b60dea08b414e
Steed Armistead <sarmist1@uga.edu>
2015-04-26 19:12:05 -0400

    Sync

--- src/controller/getProducts.java
+++ src/controller/getProducts.java
@@ -9,16 +9,14 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
 import dbHelpers.ReadQuery;
-
 import dbHelpers.UpdateQuery;
 /**
  * Servlet implementation class getProducts
  */
 @WebServlet("/getProducts")
 public class getProducts extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
@@ -28,43 +26,30 @@ public class getProducts extends HttpServlet {
         
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		this.doPost(request, response);
-		
-	}
+/**
+* @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+*/
+protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+this.doPost(request, response);
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+}
 
-		String SKU = request.getParameter("SKU");
-		String type = request.getParameter("type");
-		String flavor = request.getParameter("flavor");
-		String cost = request.getParameter("cost");
-		String price = request.getParameter("price");
-		String quantity = request.getParameter("quantity");
-				
-		UpdateQuery.doUpdate();
-		
-		String url = "/getShoppingCart";
+/**
+* @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+*/
+protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-	
-		ReadQuery rq = new ReadQuery("shopping","root","");
-		
-		rq.doRead();
-		
-		String view = rq.getHTMLTable();
-		request.setAttribute(view, "view");
-			
-		String url = "products.jsp";
+ReadQuery rq = new ReadQuery("shopping","root","");
 
-		
-		RequestDispatcher dispatcher = request.getRequestDispatcher(url);
-		dispatcher.forward(request, response);
-	}
+rq.doRead();
+
+String view = rq.getHTMLTable();
+request.setAttribute(view, "view");
+
+String url = "products.jsp";
+
+RequestDispatcher dispatcher = request.getRequestDispatcher(url);
+dispatcher.forward(request, response);
+}
 
 }
