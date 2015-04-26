24bfe580a91b2fdb5ad5fca7ba4426ee104300cc
Steed Armistead <sarmist1@uga.edu>
2015-04-26 15:23:14 -0400

    Commit to the G

--- src/controller/getProducts.java
+++ src/controller/getProducts.java
@@ -1,12 +1,15 @@
 package controller;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import dbHelpers.UpdateQuery;
 /**
  * Servlet implementation class getProducts
  */
@@ -19,21 +22,34 @@ public class getProducts extends HttpServlet {
      */
     public getProducts() {
         super();
-        // TODO Auto-generated constructor stub
+        
     }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		this.doPost(request, response);
+		
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		String SKU = request.getParameter("SKU");
+		String type = request.getParameter("type");
+		String flavor = request.getParameter("flavor");
+		String cost = request.getParameter("cost");
+		String price = request.getParameter("price");
+		String quantity = request.getParameter("quantity");
+				
+		UpdateQuery.doUpdate();
+		
+		String url = "/getShoppingCart";
+		
+		RequestDispatcher dispatcher = request.getRequestDispatcher(url);
+		dispatcher.forward(request, response);
 	}
 
 }
